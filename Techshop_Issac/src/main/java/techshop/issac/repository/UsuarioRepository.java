902456a62f6619a43016261fbd818cfd5837b3b5
IssacSegura <issacsegura2005@gmail.com>
2026-07-27 20:02:52 -0600

    semana 11

--- Techshop_Issac/src/main/java/techshop/issac/repository/UsuarioRepository.java
+++ Techshop_Issac/src/main/java/techshop/issac/repository/UsuarioRepository.java
@@ -4,10 +4,22 @@ import techshop.issac.domain.Usuario;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     
-    public Optional<Usuario> findByUsernameAndActivoTrue(String username);
     
+    public Optional<Usuario> findByUsernameAndActivoTrue(String username);
+
+    public List<Usuario> findByActivoTrue();
+
+    public Optional<Usuario> findByUsername(String username);
+
+    public Optional<Usuario> findByUsernameAndPassword(String username, String Password);
+
+    public Optional<Usuario> findByUsernameOrCorreo(String username, String correo);
+
+    public boolean existsByUsernameOrCorreo(String username, String correo);
+
 }
